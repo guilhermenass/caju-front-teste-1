86e0fe5c3fcb6e5196abf01de0577c1b3d5d22f0
Guilherme Nass <nassguilherme@gmail.com>
2024-10-21 18:25:59 -0300

    feature: fetch registrations by document

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -5,16 +5,34 @@ import { IconButton } from "~/components/Buttons/IconButton";
 import TextField from "~/components/TextField";
 import routes from "~/router/routes";
 import * as S from "./styles";
-export const SearchBar = () => {
+import { SearchbarProps, VALID_DOCUMENT_LENGTH } from "./types";
+
+export const SearchBar = ({ onSearch }: SearchbarProps) => {
   const history = useHistory();
 
   const goToNewAdmissionPage = () => {
     history.push(routes.newUser);
   };
-  
+
+  const handleDocumentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const document = event.target.value;
+
+    if (document.length === VALID_DOCUMENT_LENGTH) {
+      onSearch(document);
+    }
+
+    if (document.length === 0) {
+      onSearch();
+    }
+  };
+
   return (
     <S.Container>
-      <TextField  placeholder="Digite um CPF válido" />
+      <TextField
+        placeholder="Digite um CPF válido"
+        onChange={handleDocumentChange}
+      />
+
       <S.Actions>
         <IconButton aria-label="refetch">
           <HiRefresh />
