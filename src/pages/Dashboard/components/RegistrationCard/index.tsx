ba6249377b7c56f658c9df3379bb5f21eda60e3a
Guilherme Nass <nassguilherme@gmail.com>
2024-10-21 22:57:31 -0300

    wip: registration actions to update status

--- src/pages/Dashboard/components/RegistrationCard/index.tsx
+++ src/pages/Dashboard/components/RegistrationCard/index.tsx
@@ -6,12 +6,44 @@ import {
   HiOutlineCalendar,
   HiOutlineTrash,
 } from "react-icons/hi";
+import usePutRequest from "~/hooks/usePutRequest";
+import { Registration } from "~/models/registration";
 
-type Props = {
-  data: any;
+type RegistrationCardProps = {
+  data: Registration;
 };
 
-const RegistrationCard = (props: Props) => {
+const RegistrationCard = ({
+  data: { admissionDate, cpf, email, employeeName, id, status },
+}: RegistrationCardProps) => {
+  const { sendPutRequest } = usePutRequest();
+
+  const handleReproveRegistration = (payload: Registration) => {
+    sendPutRequest(`registrations/${id}`, payload);
+  };
+
+  const handleApproveRegistration = (id: string) => {
+    const payload = {
+      admissionDate: "22/10/2023",
+      email: "filipe@caju.com.br",
+      employeeName: "Filipe Marins",
+      status: "APPROVED",
+      cpf: "78502270001",
+    };
+    sendPutRequest(`registrations/${id}`, payload);
+  };
+
+  const handleReviewAgain = (id: string) => {
+    const payload = {
+      admissionDate: "22/10/2023",
+      email: "filipe@caju.com.br",
+      employeeName: "Filipe Marins",
+      status: "REVIEW",
+      cpf: "78502270001",
+    };
+    sendPutRequest(`registrations/${id}`, payload);
+  };
+
   return (
     <S.Card>
       <S.IconAndText>
@@ -27,9 +59,24 @@ const RegistrationCard = (props: Props) => {
         <span>{props.data.admissionDate}</span>
       </S.IconAndText>
       <S.Actions>
-        <ButtonSmall bgcolor="rgb(255, 145, 154)" >Reprovar</ButtonSmall>
-        <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
-        <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
+        <ButtonSmall
+          onClick={() => handleReproveRegistration(props.data.id)}
+          bgcolor="rgb(255, 145, 154)"
+        >
+          Reprovar
+        </ButtonSmall>
+        <ButtonSmall
+          onClick={() => handleApproveRegistration(props.data.id)}
+          bgcolor="rgb(155, 229, 155)"
+        >
+          Aprovar
+        </ButtonSmall>
+        <ButtonSmall
+          onClick={() => handleReviewAgain(props.data.id)}
+          bgcolor="#ff8858"
+        >
+          Revisar novamente
+        </ButtonSmall>
 
         <HiOutlineTrash />
       </S.Actions>
