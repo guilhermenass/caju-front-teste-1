e21c52de48cee02965863054c17bc9b4ee5f8871
Guilherme Nass <nassguilherme@gmail.com>
2024-10-22 23:49:45 -0300

    feature: call toast container

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
-import * as React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const root = createRoot(document.getElementById("root") as HTMLElement);
+import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
-
-root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <ToastContainer />
+    <App />
+  </QueryClientProvider>
 );
