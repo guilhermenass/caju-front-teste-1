b722a80ae69b190a95b744848d5a3e2b1903fc0d
Guilherme Nass <nassguilherme@gmail.com>
2024-10-24 05:59:40 -0300

    feature: using ref and styling error message

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,38 +1,21 @@
-import { InputHTMLAttributes } from "react";
-import styled from "styled-components";
+import { forwardRef, InputHTMLAttributes } from "react";
+import * as S from './styles'
 
-export const Input = styled.input`
-  padding: 0 8px;
-  vertical-align: middle;
-  border-radius: 2px;
-  width: 100%;
-  min-height: 36px;
-  background-color: #ffffff;
-  border: 1px solid rgba(36, 28, 21, 0.3);
-  transition: all 0.2s ease-in-out 0s;
-  font-size: 16px;
-  line-height: 18px;
-  font-weight: normal;
-  border-radius:8px;
-  :focus {
-    outline: none;
-    border: 1px solid #007c89;
-    box-shadow: inset 0 0 0 1px #007c89;
-  }
-`;
 type TextFieldProps = {
   label?: string;
   error?: string;
 } & InputHTMLAttributes<any>;
 
-const TextField = (props: TextFieldProps) => {
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => {
   return (
     <div>
       <label htmlFor={props.id}>{props.label}</label>
-      <Input {...props} />
-      <span style={{fontSize: 12, color: 'red'}}>{props.error}</span>
+      <S.Input {...props} ref={ref} />
+      {props.error && (
+        <S.ErrorMessage>{props.error}</S.ErrorMessage>
+      )}
     </div>
   );
-};
+});
 
 export default TextField;
