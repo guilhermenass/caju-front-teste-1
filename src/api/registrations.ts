78b22bc4efb4e5e870f63968aebbd5fbbcb6bfdf
Guilherme Nass <nassguilherme@gmail.com>
2024-10-24 05:59:22 -0300

    refact: use base url for api + create registration

--- src/api/registrations.ts
+++ src/api/registrations.ts
@@ -1,8 +1,10 @@
 import { Registration } from "~/models/registration";
 import instance from "./axios";
 
+const BASE_URL = '/registrations'
+
 export async function fetchAllRegistrations(): Promise<Registration[]> {
-  const response = await instance.get<Registration[]>("/registrations");
+  const response = await instance.get<Registration[]>(BASE_URL);
   return response.data;
 }
 
@@ -10,7 +12,7 @@ export async function fetchRegistrationsByDocument(
   document: string
 ): Promise<Registration[]> {
   const response = await instance.get<Registration[]>(
-    `/registrations?cpf=${document}`
+    `${BASE_URL}?cpf=${document}`
   );
   return response.data;
 }
@@ -19,12 +21,17 @@ export async function updateRegistration(
   id: string,
   payload: Registration
 ): Promise<Registration> {
-  const url = `/registrations/${id}`
+  const url = `${BASE_URL}/${id}`
   const response = await instance.put<Registration>(url, payload);
   return response.data;
 }
 
 export async function deleteRegistration(id: string): Promise<Registration> {
-  const response = await instance.delete(`/registrations/${id}`);
+  const response = await instance.delete(`${BASE_URL}/${id}`);
   return response.data;
+}
+
+export async function createRegistration(payload: Registration): Promise<Registration> {
+  const response = await instance.post(BASE_URL, payload)
+  return response.data
 }
