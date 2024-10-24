986a24e9a0ddf554c8b73dfb337311eb02114f08
Guilherme Nass <nassguilherme@gmail.com>
2024-10-23 21:30:53 -0300

    fix: disabled refetchOnWindowFocus

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <ToastContainer />
