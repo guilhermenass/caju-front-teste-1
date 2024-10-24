ff4c5f5d547d2d4c295f898db4047238b7d2f214
Guilherme Nass <nassguilherme@gmail.com>
2024-10-24 15:22:48 -0300

    refact: import toast css on main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 
